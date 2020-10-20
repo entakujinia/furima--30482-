c252b280f4392631eb2123c8ddfa24958f97756d
entakujinia <takusnn@icloud.com>
2020-10-20 17:43:27 +0900

    商品出品機能とFactoryBotの生成

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,10 +6,10 @@ class Item < ApplicationRecord
   belongs_to_active_hash :delivery_fee
   belongs_to_active_hash :delivery_area
   has_one_attached :image
-  #belongs_to       :user
+  belongs_to       :user
   #has_one          :purchase
 
-  validates :image, :name, :description, :category, :status, :delivery_days, :delivery_fee, :delivery_area, :price, presence: true
+  validates :image, :name, :description, :category_id, :status_id, :delivery_days_id, :delivery_fee_id, :delivery_area_id, :price, presence: true
 
   
   validates :category_id, :status_id, :delivery_days_id, :delivery_fee_id, :delivery_area_id, numericality: { other_than: 1 }
---
+++ spec/factories/items.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :item do
+    association :user
+    name               { '名前' }
+    description        { '説明' }
+    category_id        { '2' }
+    status_id          { '2' }
+    delivery_fee_id    { '2' }
+    delivery_area_id   { '2' }
+    delivery_days_id   { '2' }
+    price              { 1000 }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  end
+end
