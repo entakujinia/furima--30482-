ed14616b8e2444db0a75d717537798529b888811
entakujinia <takusnn@icloud.com>
2020-10-20 13:27:20 +0900

    ActiveHashを導入とヴァリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_day
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :delivery_area
   has_one_attached :image
   #belongs_to       :user
   #has_one          :purchase
+
+  validates :image, :name, :description, :category, :status, :delivery_days, :delivery_fee, :delivery_area, :price, presence: true
+
+  
+  validates :category_id, :status_id, :delivery_days_id, :delivery_fee_id, :delivery_area_id, numericality: { other_than: 1 }
 end
 
---
+++ test/models/cあ_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CあTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
