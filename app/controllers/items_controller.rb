31202cff34af5aedd7cfb8bc5269a41efab43f33
entakujinia <takusnn@icloud.com>
2020-10-21 19:13:52 +0900

    robocopの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
-    @item = Item.order("id DESC")
+    @item = Item.order('id DESC')
   end
 
   def new
@@ -22,9 +22,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.user != current_user
-      redirect_to root_path
-    end
+    redirect_to root_path if @item.user != current_user
   end
 
   def update
@@ -37,24 +35,18 @@ class ItemsController < ApplicationController
 
   def destroy
     if @item.user_id = current_user.id
-    @item.destroy
-    redirect_to root_path
+      @item.destroy
+      redirect_to root_path
     end
-    
   end
-
-
-
 
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :delivery_fee_id, :delivery_area_id, :delivery_day_id, :price).merge(user_id: current_user.id)
   end
-  
+
   def set_item
     @item = Item.find(params[:id])
   end
 end
-
-
