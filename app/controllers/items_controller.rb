1a0817932f92a6bc1ea70a6afbb8e2f629d7d48e
entakujinia <takusnn@icloud.com>
2020-10-21 17:49:02 +0900

    コントローラーをみやすく改善

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update]
-
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @item = Item.order("id DESC")
   end
@@ -19,19 +19,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
-
   end
 
   def edit
-    @item = Item.find(params[:id])
     if @item.user != current_user
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -46,4 +42,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :delivery_fee_id, :delivery_area_id, :delivery_day_id, :price).merge(user_id: current_user.id)
   end
+  
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
